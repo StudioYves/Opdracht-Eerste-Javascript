dff34e5978d694d9ddf6c31516cbf095429ea79e
Ivo Kersten <82444976+StudioYves@users.noreply.github.com>
2021-05-31 10:56:31 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+console.log('Hello Winc Academy');
+let name = 'Ivo Kersten';
+
+console.log(5 + 7);
+
+let variableOne = '4';
+let variableTwo = '23';
+
+console.log(variableOne + variableTwo);
+
+console.log(3 + 8);
+
+console.log(13 - 3);
+
+console.log(5 * 4);
+
+console.log(54 / 9);
+
+console.log(34 % 8);
+
+let leeftijd = 42;
+console.log(typeof leeftijd);
